396f7598c19e03ea748e88198a8795b4dffda0c4
nuyun12 <nurunnisa0098@gmail.com>
2025-03-10 13:05:41 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border ="1" cellspacing="0" cellpadding="5">
+        <thead>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323076</td>
+                <td>Nurun Nissa Ayu</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323077</td>
+                <td>Nuyun</td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
